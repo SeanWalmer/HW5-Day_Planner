8ab33a55a85435c343f426c11240b04b444211fa
SeanW <sean.walmer@outlook.com>
2020-04-12 15:59:18 -0400

    Added the color render functionallity. commented out all debug console.logs. removed test folder. removed class read me. edited read me with brief overview.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # HW5-Day_Planner
 day planner homework
+
+Day planner will create a local storage for events if one does not exist. If it does it will render them to time sections.
+
+Each time section has a coresponding save button that will save and edited or added value to the correct section in the local storage
+
+page will also render colors to help you identify the current time section, past sections and upcomming sections
--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 // sets date at top of page and grabs time for use later
 var dateGrab = new Date().toString();
 var dateArray = dateGrab.split(" ");
+// seperates time from date and pulls out just numbers for conparison in rendering colors
 var time = dateArray[4].match(/\d+/g).map(Number);
-console.log(time);
-var events
+// console.log(time);
+var events;
 
 
-$("#currentDay").text(dateArray[0] + ", " + dateArray[1] + " " + dateArray[2])
+$("#currentDay").text(dateArray[0] + ", " + dateArray[1] + " " + dateArray[2]);
 
 
 // render events to page or create storage for events if no event array exists
 function renderEvents(){
-    events = JSON.parse(localStorage.getItem("events"))
+    events = JSON.parse(localStorage.getItem("events"));
 
     if( events === null){
         events = [{
@@ -53,7 +54,8 @@ function renderEvents(){
         ];
         localStorage.setItem("events", JSON.stringify(events));
     } else{ 
-        console.log("local storage exists so I render events to page");
+        // if local storage exists it has been set to events var and is rendered to page.
+        // console.log("local storage exists so I render events to page");
         for(var i = 0; i < events.length; i++){
             $("#event"+(i+1)).attr("value", events[i].event);
         };
@@ -62,14 +64,51 @@ function renderEvents(){
 
 renderEvents();
 
+
+// function renders colors by comparing hour to time value in p tag of each section
+// values are set to 24 hour time to match given time by browser
+function renderColors(){
+    for(var i = 0; i < events.length; i++){
+        var selector = $("#event"+(i+1));
+
+        if(selector[0].previousElementSibling.attributes[1].value < time[0]){
+
+            selector[0].classList.add("bg-secondary");
+            selector[0].previousElementSibling.classList.add("bg-secondary");
+            selector[0].classList.add("text-white");
+            selector[0].previousElementSibling.classList.add("text-white");
+
+        }else if(selector[0].previousElementSibling.attributes[1].value == time[0]){
+
+            selector[0].classList.add("bg-success");
+            selector[0].previousElementSibling.classList.add("bg-success");
+            selector[0].classList.add("text-white");
+            selector[0].previousElementSibling.classList.add("text-white");
+
+
+        }else{
+
+            selector[0].classList.add("bg-primary");
+            selector[0].previousElementSibling.classList.add("bg-primary");
+            selector[0].classList.add("text-white");
+            selector[0].previousElementSibling.classList.add("text-white");
+
+
+        };
+        // debug colsole log to make sure I know which paths to grab
+        // console.log(selector);
+    };
+};
+
+renderColors();
+
 // event listener for save buttons
 $(".btn").click(function(){
-    // console.log("you clicked a save button");
     var btnNum = this.id.match(/\d+/g).map(Number);
-    console.log("you clicked save button " + btnNum[0]);
-    console.log("you saved '" + this.previousElementSibling.value +"' to position " + (btnNum-1) + "in events array.")
+    // console.log("you clicked save button " + btnNum[0]);
+    // console.log("you saved '" + this.previousElementSibling.value +"' to position " + (btnNum-1) + "in events array.");
     events[btnNum - 1].event = this.previousElementSibling.value;
-    console.log(events)
-    console.log("saving to local storage")
+    // console.log(events);
+    // console.log("saving to local storage");
     localStorage.setItem("events", JSON.stringify(events));
-})
+});
