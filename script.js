800c570bb6cf94d8350d4bea1f2c9fef203b4d7d
SeanW <sean.walmer@outlook.com>
2020-04-11 21:08:58 -0400

    Added id's to buttons and input fields so JS can grab them. Added JS to create storage and render events (event rendering is just a console log atm to test and see if the if/else statment works

--- script.js
+++ script.js
@@ -1,7 +1,63 @@
 // sets date at top of page and grabs time for use later
 var dateGrab = new Date().toString();
-console.log(dateGrab)
 var dateArray = dateGrab.split(" ")
-console.log(dateArray)
+var time = dateArray[4]
+console.log(time)
+
 
 $("#currentDay").text(dateArray[0] + ", " + dateArray[1] + " " + dateArray[2])
+
+
+// render events to page or create storage for events if no event array exists
+function renderEvents(){
+    var events = JSON.parse(localStorage.getItem("events"))
+
+    if( events === null){
+        events = [{
+            // 1
+            event: ""
+        },
+        {
+            // 2
+            event: ""
+        },
+        {
+            // 3
+            event: ""
+        },
+        {
+            // 4
+            event: ""
+        },
+        {
+            // 5
+            event: ""
+        },
+        {
+            // 6
+            event: ""
+        },
+        {
+            // 7
+            event: ""
+        },
+        {
+            // 8
+            event: ""
+        },
+        {
+            // 9
+            event: ""
+        }
+        ];
+        localStorage.setItem("events", JSON.stringify(events))
+    } else{ 
+        console.log("local storage exists so I render events to page")
+    }
+}
+
+renderEvents()
+
+// function renderEvents(){
+
+// }
